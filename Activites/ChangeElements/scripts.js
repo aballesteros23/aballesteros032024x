62b4d6bfd9ee7860db8e00cbfeb2b41089ce5cc7
Aldo Ballesteros Torrez <aball-s966@Staff-KQPX64WVP3.local>
2025-05-19 11:18:18 -0700

    styles

---
+++ Activites/ChangeElements/scripts.js
@@ -0,0 +1,57 @@
+let selStylesheet=document.querySelector('#selstylesheet');
+let styleLink=document.querySelector('#stylelink');
+let mainImage = document.querySelector('#mainimage');
+let headerQuote = document.querySelector('#quote');
+
+
+
+function updateStylesheet() {
+
+
+        styleLink.href=selStylesheet.value;
+        // changeSong();
+        localStorage.setItem('selectedStylesheet19448', selStylesheet.value);
+        // alert(selStylesheet.value)
+
+
+        // Change blockquote
+        changeElements();
+
+}
+
+
+window.addEventListener('load', function () {
+        let savedStylesheet;
+        savedStylesheet=localStorage.getItem('selectedStylesheet19448');
+
+        /*** && Update for changed stylesheet names */
+        if (savedStylesheet !==null && savedStylesheet.value == styleLink.attributes.href.value) {
+
+            styleLink.href=savedStylesheet;
+            selStylesheet.value=savedStylesheet;
+            
+        }
+
+
+    });
+
+    function changeElements()
+    {
+       let currentStylesheet = styleLink.attributes.href.value;
+
+    //    alert(currentStyleSheet);
+
+       if(currentStylesheet == "redtheme.css")
+       {
+            mainImage.src = "Images/redsquare.png";
+            headerQuote.innerHTML ="<h1>Hello</h1>";
+       }
+
+       if(currentStylesheet == "greentheme.css")
+       {
+            mainImage.src = "Images/greensquare.png";
+            headerQuote.innerHTML ="<h1>Goodbye</h1>";
+       }
+    }
+
+
